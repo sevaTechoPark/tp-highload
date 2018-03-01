e4cda282be77b0d353d679860ae24cd5c851a61b
seva <seva213415@inbox.ru>
2018-03-01 06:19:26 +0300

    move config file

--- scr/main.cpp
+++ scr/main.cpp
@@ -8,31 +8,37 @@ int main() {
         string root_dir;
         int threadsCount;
 
-        std::ifstream in("../httpd.conf");
-        char* c = new char[100];
-        std::streamsize n = 100;
+        std::ifstream in("/etc/httpd.conf");
+        if (in.is_open()) {
+            char* c = new char[100];
+            std::streamsize n = 100;
 
-        for (size_t i = 0; i < 3 && in.good(); i++) {
-            in.getline(c, n);
-            string data(c);
-            std::size_t found = data.find_last_of('=');
-            data = data.substr(found + 2);
+            for (size_t i = 0; i < 3 && in.good(); i++) {
+                in.getline(c, n);
+                string data(c);
+                std::size_t found = data.find_last_of('=');
+                data = data.substr(found + 2);
 
-            switch (i) {
-                case 0:
-                    port = std::atoi(data.c_str());
-                    break;
-                case 1:
-                    root_dir = data;
-                    break;
-                case 2:
-                    threadsCount = std::atoi(data.c_str());
-                    break;
+                switch (i) {
+                    case 0:
+                        port = std::atoi(data.c_str());
+                        break;
+                    case 1:
+                        root_dir = data;
+                        break;
+                    case 2:
+                        threadsCount = std::atoi(data.c_str());
+                        break;
+                }
             }
-        }
 
-        in.close();
-        delete (c);
+            in.close();
+            delete (c);
+        } else {
+            port = 8000;
+            root_dir = "/home/seva/dev/technopark/http-test-suite";
+            threadsCount = 2;
+        }
 
         Server server(port, root_dir, threadsCount);
         server.start();
